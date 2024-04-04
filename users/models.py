611f4999e8fcb5a8635b902e0ecf87a3d127b92c
firmprojects <kcvontop@gmail.com>
2024-04-04 10:12:07 +0100

    cmmt

--- users/models.py
+++ users/models.py
@@ -52,4 +52,4 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _("users")
 
     def __str__(self):
-        return self.email if self.email else self.phone
+        return self.email if self.email else "No email set"
