37bfb88f785e7f2c621ca6c5612d7bb71b03a3cb
firmprojects <kcvontop@gmail.com>
2024-07-23 08:08:27 +0100

    cmmt

--- appauth/settings.py
+++ appauth/settings.py
@@ -248,16 +248,6 @@ CELERY_BEAT_SCHEDULE = {
     }
 }
 
-# CLOUDINARY_CLOUD_NAME = getenv("CLOUDINARY_CLOUD_NAME")
-# CLOUDINARY_API_KEY = getenv("CLOUDINARY_API_KEY")
-# CLOUDINARY_API_SECRET = getenv("CLOUDINARY_API_SECRET")
-
-# cloudinary.config(
-#     cloud_name=CLOUDINARY_CLOUD_NAME,
-#     api_key=CLOUDINARY_API_KEY,
-#     api_secret=CLOUDINARY_API_SECRET,
-# )
-
 
 STATIC_URL = "static/"
 
